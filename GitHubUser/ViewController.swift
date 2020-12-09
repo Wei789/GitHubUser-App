74d055cd779783a42a5f374f61ebc10d9bce86fb
鄭惟臣 <weichen.cheng@thinkpower-info.com>
2020-12-09 21:45:23 +0800

    Initial Commit

---
+++ GitHubUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUser
+//
+//  Created by 鄭惟臣 on 2020/12/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
